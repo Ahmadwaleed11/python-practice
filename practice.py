b0a81d87a5981808d3ae36cecfceac340290680a
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-05 09:48:50 +0500

    update code while taking tea

--- practice.py
+++ practice.py
@@ -4,3 +4,10 @@ print(len(name))
 
 print(name.upper())
 print(name.rstrip("a"))
+head='my nam is harry'
+print(head.capitalize())
+print(head.count('a'))
+print(head.find('a'))
+
+print(head.endswith('harry'))
+print(head.startswith('my'))
