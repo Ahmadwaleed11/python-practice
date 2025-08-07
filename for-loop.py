61ee47215c58278cf438cf5c7bfca7549e19f90a
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-07 10:54:14 +0500

    while,break,continue

--- for-loop.py
+++ for-loop.py
@@ -1,2 +1,2 @@
-for k in range(10):
+for k in range(1,10,2):
     print(k)
---
+++ while-loop.py
@@ -0,0 +1,7 @@
+for i in  range(20):
+    print(i)
+
+    if(i==10):
+       print('skip the iteration')
+       break
+print("done")    
