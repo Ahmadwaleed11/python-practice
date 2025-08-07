82d6ba350441c46a10f2b41c09945030d6ac6897
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-07 22:34:49 +0500

    enter new code

---
+++ def.py
@@ -0,0 +1,16 @@
+def sum(x,y):
+    num=x+y
+    print(num)
+
+
+
+
+
+
+x=4
+y=5
+sum(x,y)
+
+
+print('hello')
+
--- while-loop.py
+++ while-loop.py
@@ -2,6 +2,16 @@ for i in  range(20):
     print(i)
 
     if(i==10):
+
        print('skip the iteration')
        break
+     
+       
 print("done")    
+
+# while True:
+#     num=int(input("enter the number"))
+#     print(num)
+#     if(num>0):
+#         print("done")
+#         break
