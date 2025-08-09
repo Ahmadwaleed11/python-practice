a6d182228ced83b1c53ad9385a5716fa58d2ed67
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-09 10:36:01 +0500

    learning-diff-datatypes-of-list

--- list.py
+++ list.py
@@ -1,5 +1,32 @@
 ############# Append method ###########
-l=[1,2,3,4,5,6]
-print(l)    
-l.append(8)
+# l=[1,2,3,4,5,6]
+# print(l)    
+# l.append(8)
+# print(l)
+
+
+
+
+
+# ############  Sort ############3######
+# l=[12,2,3,45,6,7,12]
+# print(l)
+# l.sort(
+# )
+# print(l)
+
+# ############ reverse true       #################
+# l.sort(reverse=True)
+# print(l)
+
+
+l=[12,23,5,23,4]
+# print(l.count(12))
+# print(l.index(23))
+# l.insert(2,2445)
+m=[12,223,456]
+l.extend(m)
 print(l)
+
+
+
