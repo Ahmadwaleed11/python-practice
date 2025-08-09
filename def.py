2dfe9dcc312ad31ee7f62e660f86eaec578f6e0a
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-09 10:19:53 +0500

    list-method

--- def.py
+++ def.py
@@ -1,8 +1,19 @@
-def average(*number):
-    sum=0
-    for i in number:
-        sum=sum+i
-        print('average is ', sum/len(number))
+# l=['ahmad','waleed','harry',3,4,5]
+# print(l[2])
+# print(l[1])
+# print(l[-4])
+
+# if "harry" in l:
+#     print('yes')
+# else:
+#     print('no')    
 
 
-average(2,4,5,6)
+
+lst=[i*i for i in range(10) if i%2==0] 
+print(lst)
+
+# marks=[1,2,3,4,5,6,7,8]
+# print(marks)
+# print(marks[0:len(marks)])
+# print(marks[0:6:2])
---
+++ list.py
@@ -0,0 +1,5 @@
+############# Append method ###########
+l=[1,2,3,4,5,6]
+print(l)    
+l.append(8)
+print(l)
