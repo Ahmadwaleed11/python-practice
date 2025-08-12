6648a921e11ff3aa7a707c21a03928b17c823cb6
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-12 10:30:24 +0500

    changes in string

--- string.py
+++ string.py
@@ -2,4 +2,6 @@ line='my name is {} and i am fom {}'
 name="ahmad"
 country="pakistan"
 
-print(line.format(name,country))
+# print(line.format(name,country))
+
+print(f'my name is {name} and i am fom {country}')
