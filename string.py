ca38ab6f22152ff61d823240e810451a48cfef5d
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-12 10:28:09 +0500

    f-string

---
+++ string.py
@@ -0,0 +1,5 @@
+line='my name is {} and i am fom {}'
+name="ahmad"
+country="pakistan"
+
+print(line.format(name,country))
---
+++ tuple.py
@@ -0,0 +1,8 @@
+################# operation on tuple ################
+
+
+tuple=(1,2,3,3,4,5,2,3,2,3,2,3)
+# res=tuple.count(2)
+# res=tuple.index(5)
+res=tuple.index(2,3,8)
+print(res)
