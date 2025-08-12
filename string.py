19b037ee8691414fd99fad5c14093038c681b990
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-12 10:58:04 +0500

    doc-string

--- string.py
+++ string.py
@@ -1,7 +1,20 @@
-line='my name is {} and i am fom {}'
-name="ahmad"
-country="pakistan"
+############ f-string ##########
 
-# print(line.format(name,country))
 
-print(f'my name is {name} and i am fom {country}')
+# line='my name is {} and i am fom {}'
+# name="ahmad"
+# country="pakistan"
+
+# # print(line.format(name,country))
+
+# print(f'we use f-string:my name is {name} and i am fom {country}')
+
+
+
+############# doc-string ###########
+
+
+def square(n):
+    '''this is square function'''
+    print(n**2)
+square(5)
