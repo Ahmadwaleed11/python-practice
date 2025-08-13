f119d20666160dcccf7eb23973fe3733c98d03e5
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-13 10:46:49 +0500

    intersection-set

--- set.py
+++ set.py
@@ -12,3 +12,12 @@
 # print(s1.union(s2))
 # s1.update(s2)
 # print(s1,s2)
+
+
+########## intersection set ##################
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+print(s1.intersection(s2))
+s1.intersection_update(s2)
+print(s1,s2)
