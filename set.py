83c78e1ecf8e84515e2fa6b21fe8206608e21967
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-13 10:43:01 +0500

    union-set

---
+++ set.py
@@ -0,0 +1,14 @@
+# s={'ahmad','waleed','harry','ahmad','waleed','harry','ahmad','waleed','harry','ahmad','waleed','harry'}
+# print(s)
+
+# name=set()
+# print(type(name))
+
+
+####### union set #####################
+
+# s1={1,2,3,4}
+# s2={3,4,5,6}
+# print(s1.union(s2))
+# s1.update(s2)
+# print(s1,s2)
