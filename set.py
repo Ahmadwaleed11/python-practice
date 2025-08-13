fea186117fa364026c7a879f41331f839e2ac0db
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-13 10:51:56 +0500

    symmetric-set

--- set.py
+++ set.py
@@ -16,8 +16,16 @@
 
 ########## intersection set ##################
 
+# s1={1,2,3,4}
+# s2={3,4,5,6}
+# print(s1.intersection(s2))
+# s1.intersection_update(s2)
+# print(s1,s2)
+
+########## symmetric difference set ##################
+
 s1={1,2,3,4}
 s2={3,4,5,6}
-print(s1.intersection(s2))
-s1.intersection_update(s2)
-print(s1,s2)
+# print(s1.symmetric_difference(s2))
+# print(s1.symmetric_difference_update(s2))
+print(s1.difference(s2))
