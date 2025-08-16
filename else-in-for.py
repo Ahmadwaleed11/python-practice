c7e0ca2040659804608dc291152a1e234e99301c
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-16 10:54:26 +0500

    break-in loop

--- else-in-for.py
+++ else-in-for.py
@@ -1,4 +1,16 @@
-for i in []:
+# for i in range(6):
+#     print(i)
+#     if (i==4):
+#        print('helllo')
+#     continue
+   
+# else:    
+#     print('sorry')
+
+i=0
+while i<7:
     print(i)
-else:    
-    print('sorry')
+    i=i+1
+    if (i==4):
+     print('hello')
+     continue
