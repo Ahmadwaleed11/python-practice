b49ef25b9dd8f692772cc90323b61fb28934b3bc
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-18 11:00:02 +0500

    enumerate func

--- short-hand.py
+++ short-hand.py
@@ -1,4 +1,27 @@
-a=234
-b=123
-print('a') if a>b else print('unless') if a==b  else print('b') 
-print(23) if a<b else ''
+# a=234
+# b=123
+# print('a') if a>b else print('unless') if a==b  else print('b') 
+# print(23) if a<b else ''
+
+
+####### Enumerate function #########$##
+
+# marks=[12,23,45,12,34]
+
+# index=0
+# for mark in marks:
+#     print(mark)
+#     if (index==2):
+#      print('ahmad')
+#     index=index+1
+
+# marks=[12,23,45,12,34]
+# for index,mark in enumerate(marks):
+#     print(mark)
+#     if (index==2):
+#      print(index,mark)
+
+
+fruit=['apple','banana','orange','peach']
+for index,fruit in enumerate(fruit):
+    print(index,fruit)
