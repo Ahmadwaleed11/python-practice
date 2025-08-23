0a7276ac5b843b66e5011f7629fcc2f9b8bb2628
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-23 10:28:44 +0500

    filter

--- map,filter,reduce.py
+++ map,filter,reduce.py
@@ -1,15 +1,27 @@
-def cube(x):
-    return x*x*x
-print(cube(3))
+# def cube(x):
+#     return x*x*x
+# print(cube(3))
 
-l=[2,3,4,5,6]
-# newl=[]
-# for item in l:
-#     newl.append(cube(item))
+# l=[2,3,4,5,6]
+# # newl=[]
+# # for item in l:
+# #     newl.append(cube(item))
 
-# print(newl)  
+# # print(newl)  
  
 
 
-newl=list(map(cube,l) )
+# newl=list(map(cube,l) )
+# print(newl)
+
+
+######### filter ######################
+
+def filter_func(x):
+    return x>23
+
+l=[22,3,53,33,34,5]
+
+newl=list (filter(filter_func,l))
 print(newl)
+
