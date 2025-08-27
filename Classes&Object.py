21ad936e73515f0102a75de3db546e1a72a7d55c
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-27 20:53:00 +0500

    sefl:object jo function ko call kry

--- Classes&Object.py
+++ Classes&Object.py
@@ -1,8 +1,13 @@
 class student:
     name='ahmad'
     rollno=23
+    def info(self):
+        print(f"{self.name} and its roll no is {self.rollno}")
   
 a=student()
-# a.name='waleed'  ###### if we want to change value
-# a.rollno=45
-print(a.name , a.rollno)  
+b=student()
+a.name='waleed'  ###### if we want to change value
+a.rollno=45
+# print(a.name , a.rollno)  
+a.info()
+b.info()
