b44f7b32001d5f7413fe4daab6527e49241fbf89
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-28 20:49:05 +0500

    bc dimaag khrab kr dia is ny

--- Getter&Setter.py
+++ Getter&Setter.py
@@ -1,13 +1,35 @@
-class name:
-    def __init__(self,n):
-        self.name=n
+# class student:
+#    def __init__ (self,n,s):
+#      self.name=n
+#      self.sect=s
+#    def show(self):
+#        print(f"my name is {self.name } and my section is {self.sect}")
+#    @property
+#    def get_name(self):
+#          return self.name   
+# obj=student("waleed",2)
+# obj.show()
+# print(obj.get_name)
+
+class student:
+    def __init__ (self,value):
+        self.value=value
 
     def show(self):
-        print(self.name)   
-
+        print(f"value is {self.value}") 
     @property
-    def info(self):
-        return 5*self.name         
-a=name(23)
-a.show()
-print(a.info)
+    def get_value(self):
+        return 10*self.value    
+    @get_value.setter
+    def get_value(self,new_value):
+        self.value=new_value/10
+    
+    
+obj=student(12)  
+obj.get_value=23     
+
+print(obj.get_value)  
+obj.show()
+
+
+
