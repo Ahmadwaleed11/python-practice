72dfcf956b63f8853286b4f33534492597e851aa
Ahmadwaleed11 <worldharry9@gmail.com>
2025-08-30 21:05:15 +0500

    access modifer (private)

--- Acces_modif.py
+++ Acces_modif.py
@@ -1,7 +1,15 @@
 # class employee:
-#     def __init__(self):             //// Public class
+#     def __init__(self):             //// Public class 
 #         self.name="harry"
    
 # a=employee()        
 # print(a.name)
+
+
+class employee:
+    def __init__(self):             
+        self.__name="harry"        # private classs / can be access indirectly
+   
+a=employee()        
+print(a._employee__name)
 
