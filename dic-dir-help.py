b8914db0fe1e0811e2a9389333e1150a2a11ab95
Ahmadwaleed11 <worldharry9@gmail.com>
2025-09-05 10:15:15 +0500

    dir

--- dic-dir-help.py
+++ dic-dir-help.py
@@ -1,3 +1,13 @@
-x=[1,2,3,4]
-print(dir(x))
-print(x.__add__)
+# x=(1,2,3,4)
+# print(dir(x))
+
+class student:
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+        self.roll=34
+obj=student('ahmad',34)  
+
+print(obj.__dict__)
+   
+
