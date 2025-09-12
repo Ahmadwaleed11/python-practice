59200670a1e5256cfe420c378e062764dbeffda5
Ahmadwaleed11 <worldharry9@gmail.com>
2025-09-12 09:57:28 +0500

    time module in pyhton

---
+++ timpe.py
@@ -0,0 +1,15 @@
+import time
+def show():
+    for  i in range (100):
+        print(i)
+def sh():
+    i=0
+    while i<50:
+        print(i)
+        i=i+1
+init=time.time()      
+show()
+print(time.time()- init)        
+init=time.time()      
+sh()
+print(time.time()- init)        
