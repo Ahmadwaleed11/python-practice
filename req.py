dcf355b0044117752fb973a060c46fb2f5291723
Ahmadwaleed11 <worldharry9@gmail.com>
2025-09-16 20:34:56 +0500

    how to use beautifulsoup module

--- req.py
+++ req.py
@@ -1,12 +1,21 @@
 import requests
-# r=requests.get("https://www.google.com")
-# print(r.text)
-url="https://jsonplaceholder.typicode.com/posts"
-data={
-    "title": 'ahmad',
-    "body": 'hello',
-    "userId": 1,
-  }
-headers={'Content-Type': 'application/json; charset=UTF-8',}
-res=requests.post(url, headers=headers ,json=data)
-print(res.text)
+# # r=requests.get("https://www.google.com")
+# # print(r.text)
+# url="https://jsonplaceholder.typicode.com/posts"
+# data={
+#     "title": 'ahmad',
+#     "body": 'hello',
+#     "userId": 1,
+#   }
+# headers={'Content-Type': 'application/json; charset=UTF-8',}
+# res=requests.post(url, headers=headers ,json=data)
+# print(res.text)
+
+
+from bs4 import BeautifulSoup
+
+url="https://jsonplaceholder.typicode.com/guide/"
+r=requests.get(url)
+soup=BeautifulSoup(r.text, 'html.parser')
+for heading in soup.find_all("h3"):
+    print(heading.text)
