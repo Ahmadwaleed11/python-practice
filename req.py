6f619df25071108175d2c3e26ada40ba8faccf40
Ahmadwaleed11 <worldharry9@gmail.com>
2025-09-16 18:41:02 +0500

    post request

--- req.py
+++ req.py
@@ -1,3 +1,12 @@
 import requests
-r=requests.get("https://www.google.com")
-print(r.text)
+# r=requests.get("https://www.google.com")
+# print(r.text)
+url="https://jsonplaceholder.typicode.com/posts"
+data={
+    "title": 'ahmad',
+    "body": 'hello',
+    "userId": 1,
+  }
+headers={'Content-Type': 'application/json; charset=UTF-8',}
+res=requests.post(url, headers=headers ,json=data)
+print(res.text)
