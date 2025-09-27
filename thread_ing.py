a42b0cd3603db3827386d4dd4590d155c7b92b4c
Ahmadwaleed11 <worldharry9@gmail.com>
2025-09-27 11:07:06 +0500

    threading.thread in python

--- thread_ing.py
+++ thread_ing.py
@@ -5,7 +5,14 @@ def func(seconds):
 
     print("hello")
     time.sleep(seconds)
-func(3)    
-func(4)    
-func(2)    
+# func(3)    
+# func(4)    
+# func(2)    
     
+a1=threading.Thread(target=func,args=[2])    
+a2=threading.Thread(target=func,args=[3])    
+a3=threading.Thread(target=func,args=[4])    
+
+a1.start()
+a2.start()
+a3.start()
